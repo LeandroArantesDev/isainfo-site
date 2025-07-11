954af8a15f10ef6c7cb77d4ee2fe28bd6af90a00
oCrazyoff <walyssonribeiro3@gmail.com>
2025-07-11 12:06:49 -0300

    atualizações no footer e section de carrossel

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    }
+});
